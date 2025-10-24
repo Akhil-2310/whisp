87701b8cb9616d5f92c6469e8ebc220497541cbb
Kutman Sulaimanov <kutman.sulaimanov@alatoo.edu.kg>
2025-10-24 23:24:24 +0600

    Customize landing page and add create/browse/my-groups pages

---
+++ packages/nextjs/app/create-group/page.tsx
@@ -0,0 +1,12 @@
+/**
+ * Page for creating a new group.
+ * A form component will be added here to collect the group's details.
+ */
+export default function CreateGroup() {
+  return (
+    <main className="p-6">
+      <h1 className="text-2xl font-bold">Create Group</h1>
+      {/* TODO: Implement form for group creation */}
+    </main>
+  );
+}
---
+++ packages/nextjs/app/my-groups/page.tsx
@@ -0,0 +1,12 @@
+/**
+ * Page for displaying groups that the current user has joined.
+ * It will show only the groups where the connected wallet is a member.
+ */
+export default function MyGroups() {
+  return (
+    <main className="p-6">
+      <h1 className="text-2xl font-bold">My Groups</h1>
+      {/* TODO: Implement list of groups the user belongs to */}
+    </main>
+  );
+}
